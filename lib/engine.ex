3a98ae7b9322b1afe5ca5f521f8edec0ff66ace6
Anton Volkov <choixer@gmail.com>
2016-11-28 18:33:21 +0100

    Refactor engine code in the way that matching is easy to implement

--- lib/engine.ex
+++ lib/engine.ex
@@ -21,42 +21,55 @@ defmodule Exchange.Engine do
     {:ok, {[], []}}
   end
 
-  def handle_cast({:add, order}, book) do
-    {:noreply, add_order(book, order)}
+  def handle_cast({:add, order}, books) do
+    {:noreply, handle_add_order(books, order)}
   end
 
-  def handle_call(:book, _, book) do
-    {:reply, book, book}
+  def handle_call(:book, _, books) do
+    {:reply, books, books}
   end
 
-  ##
+  ## Private ###################################################################
 
-  defp add_order({buy, sell}, %{side: :buy} = order) do
+  defp handle_add_order({buy_side, sell_side}, %{side: :buy} = order) do
+    {sell_side, order} = match_order(sell_side, order)
+    buy_side = add_order(buy_side, order)
+    {buy_side, sell_side}
+  end
+
+  defp handle_add_order({buy_side, sell_side}, %{side: :sell} = order) do
+    {buy_side, order} = match_order(buy_side, order)
+    sell_side = add_order(sell_side, order)
+    {buy_side, sell_side}
+  end
+
+  def match_order(entries, order) do
+    case top(entries) do
+      nil ->
+        {entries, order}
+
+      {price, _orders} ->
+        if matching?(price, order) do
+          {rest(entries), Map.put(order, :size, 0)}
+        else
+          {entries, order}
+        end
+    end
+  end
+
+  def add_order(entries, %{size: 0}), do: entries
+  def add_order(entries, %{size: size} = order) when size > 0 do
     entry =
-      buy
+      entries
       |> find_entry(order)
       |> put_order(order)
 
-    buy =
-      buy
-      |> put_entry(entry)
-      |> Enum.sort(fn({p1, _}, {p2, _}) -> p1 > p2 end)
+    entries = entries |> put_entry(entry)
 
-    {buy, sell}
-  end
-
-  defp add_order({buy, sell}, %{side: :sell} = order) do
-    entry =
-      sell
-      |> find_entry(order)
-      |> put_order(order)
-
-    sell =
-      sell
-      |> put_entry(entry)
-      |> Enum.sort(fn({p1, _}, {p2, _}) -> p1 < p2 end)
-
-    {buy, sell}
+    case order do
+      %{side: :buy}   -> Enum.sort(entries, &buy_book_sort/2)
+      %{side: :sell}  -> Enum.sort(entries, &sell_book_sort/2)
+    end
   end
 
   defp find_entry(entries, %{price: price}) do
@@ -71,5 +84,24 @@ defmodule Exchange.Engine do
     else
       [entry | entires]
     end
+  end
+
+  defp top([entry | _]), do: entry
+  defp top([]),          do: nil
+
+  defp rest([_ | r]),    do: r
+  defp rest([]),         do: []
+
+
+  defp sell_book_sort({p1, _}, {p2, _}), do: p1 < p2
+
+  defp buy_book_sort({p1, _}, {p2, _}), do: p1 > p2
+
+  defp matching?(price, %{side: :buy, price: order_price}) do
+    price <= order_price
+  end
+
+  defp matching?(price, %{side: :sell, price: order_price}) do
+    price >= order_price
   end
 end
--- test/engine_test.exs
+++ test/engine_test.exs
@@ -94,4 +94,70 @@ defmodule Exchange.EngineTest do
     {_buy, sell} = Engine.book(engine)
     assert sell == [{50_00, [order_1, order_2]}]
   end
+
+  test "matching a single buy order against identical outstanding sell order" do
+    # Example(1): exact same price as top of the Sell order book
+    {:ok, engine} = Engine.start_link(:test)
+    init_orders!(engine)
+
+    Engine.add(engine, %{side: :buy, size: 100, price: 57_00})
+
+    {buy, sell} = Engine.book(engine)
+    assert [{54_00, _}, {53_00, _}] = buy
+    assert [{58_00, _}] = sell
+
+    # Example(2): higher price then the top of the Sell book
+    {:ok, engine} = Engine.start_link(:test2)
+    init_orders!(engine)
+
+    Engine.add(engine, %{side: :buy, size: 100, price: 58_00})
+
+    {buy, sell} = Engine.book(engine)
+    assert [{54_00, _}, {53_00, _}] = buy
+    assert [{58_00, _}] = sell
+  end
+
+  test "matching a single sell order against identical outstanding buy order" do
+    # Example(1): exact same price as top of the buy order book
+    {:ok, engine} = Engine.start_link(:test)
+    init_orders!(engine)
+
+    Engine.add(engine, %{side: :sell, size: 100, price: 54_00})
+
+    {buy, sell} = Engine.book(engine)
+    assert [{53_00, _}] = buy
+    assert [{57_00, _}, {58_00, _}] = sell
+
+    # Example(2): higher price then the top of the buy book
+    {:ok, engine} = Engine.start_link(:test2)
+    init_orders!(engine)
+
+    Engine.add(engine, %{side: :sell, size: 100, price: 53_00})
+
+    {buy, sell} = Engine.book(engine)
+    assert [{53_00, _}] = buy
+    assert [{57_00, _}, {58_00, _}] = sell
+  end
+
+  test "matching a buy order large enough to clear the sell book" do
+    {:ok, engine} = Engine.start_link(:test)
+    init_orders!(engine)
+
+    Engine.add(engine, %{side: :buy, size: 350, price: 58_00})
+
+    {buy, sell} = Engine.book(engine)
+    assert [{58_00, [%{size: 50}]}, {54_00, _}, {53_00, _}] = buy
+    assert [] = sell
+  end
+
+  def init_orders!(engine) do
+    Engine.add(engine, %{side: :buy, size: 100, price: 54_00})
+    Engine.add(engine, %{side: :buy, size: 200, price: 53_00})
+    Engine.add(engine, %{side: :sell, size: 100, price: 57_00})
+    Engine.add(engine, %{side: :sell, size: 200, price: 58_00})
+
+    assert {buy_book, sell_book} = Engine.book(engine)
+    assert [{54_00, _}, {53_00, _}] = buy_book
+    assert [{57_00, _}, {58_00, _}] = sell_book
+  end
 end
